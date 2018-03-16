77fa98f1958416bc8d333384c74f1e99b324d3f1
Altgear <tagir337@gmail.com>
2018-03-16 12:58:11 +0300

    cherry pick

--- sources/vector.cpp
+++ sources/vector.cpp
@@ -30,7 +30,7 @@ vector_t &vector_t::operator=(vector_t const &other)
         size_ = other.size_;
         capacity_ = other.capacity_;
         elements_ = new int[capacity_];
-        for (unsigned int i = 0; i < size; ++i)
+        for (unsigned int i = 0; i < size_; ++i)
         {
             elements_[i] = other.elements_[i];
         }
